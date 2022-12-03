19f6e15e45870a6262a70826743f22dcd211710c
Abdulrasheed <jamaalokh@gmail.com>
2022-12-03 06:44:53 +0100

    function that replaces an element in a list at a specific position without modifying the original list (like in C)

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
-
-    if (idx < 0) or (idx > (len(my_list)-1)):
+    """Replace an element at specific index without modifying the whole list"""
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
 
     copy = [x for x in my_list]
